852e141351a1a956a9d2cdb5e917552db12fbb87
Justin Garfield <github@jgarfield.com>
2023-06-26 07:44:46 -0400

    cleaning

---
+++ scripts/pki/cfssl-generate-ca.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+### Overview
+# This script waits until the dmesg logs on a particular talos node return
+# the phrase, "etcd is waiting to join the cluster". At this stage, the node
+# is ready for etcd bootstrapping to be perfomed via talosctl.
+
+### Usage
+# This script expects to be passed the first control-plane node's hostname as the only argument
+# e.g: ./scripts/talos/wait-for-etcd-ready.sh k8s-cp01
+
+### Arrrrrrrgs
+output_filename=$1
+log_level=$2 || 2
+
+#################################################
+cfssl gencert -initca=true -loglevel=$log_level - \
+    | cfssljson -bare $output_filename
+
+printf "[home-ops] CA CSR created...............................  $output_filename.csr"
+printf "[home-ops] CA Public key created........................  $output_filename.pem"
+printf "[home-ops] CA Private key created.......................  $output_filename-key.pem"
---
+++ terraform/cloudflare/variables.tf
@@ -0,0 +1,5 @@
+variable "cloudflare_zone_id" {
+  type        = string
+  description = "The Cloudflare zone_id of the domain being configured"
+  default     = ""
+}
