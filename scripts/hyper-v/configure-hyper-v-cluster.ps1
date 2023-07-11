c34b9b7f85a24bd68fa0974c69bcf4204e060e3e
Justin Garfield <github@jgarfield.com>
2023-07-11 09:55:37 -0400

    Another day5

--- scripts/hyper-v/configure-hyper-v-cluster.ps1
+++ scripts/hyper-v/configure-hyper-v-cluster.ps1
@@ -149,10 +149,10 @@ function Get-NextVMNumber {
   return $prefix
 }
 
-New-TalosVM -VMNamePrefix k8s-cp -CPUCount 4 -StartupMemory 8GB -SwitchName StagingCp -TalosISOPath "C:\Users\jgarf\Downloads\talos-amd64-v1.4.4.iso" -NumberOfVMs 1 -VMDestinationBasePath 'C:\Hyper-V\Talos' -VHDSize 20GB -MAC 001337133701
+# New-TalosVM -VMNamePrefix k8s-cp -CPUCount 4 -StartupMemory 8GB -SwitchName StagingCp -TalosISOPath "C:\Users\jgarf\Downloads\talos-amd64-v1.4.4.iso" -NumberOfVMs 1 -VMDestinationBasePath 'C:\Hyper-V\Talos' -VHDSize 20GB -MAC 001337133701
 # New-TalosVM -VMNamePrefix k8s-cp -CPUCount 4 -StartupMemory 8GB -SwitchName StagingCp -TalosISOPath "C:\Users\jgarf\Downloads\talos-amd64-v1.4.4.iso" -NumberOfVMs 1 -VMDestinationBasePath 'C:\Hyper-V\Talos' -VHDSize 20GB -MAC 001337133702
 # New-TalosVM -VMNamePrefix k8s-cp -CPUCount 4 -StartupMemory 8GB -SwitchName StagingCp -TalosISOPath "C:\Users\jgarf\Downloads\talos-amd64-v1.4.4.iso" -NumberOfVMs 1 -VMDestinationBasePath 'C:\Hyper-V\Talos' -VHDSize 20GB -MAC 001337133703
-New-TalosVM -VMNamePrefix k8s-wk -CPUCount 8 -StartupMemory 12GB -SwitchName StagingWk -TalosISOPath "C:\Users\jgarf\Downloads\talos-amd64-v1.4.4.iso" -NumberOfVMs 1 -VMDestinationBasePath 'F:\Hyper-V\Talos' -VHDSize 50GB -StorageVHDSize 100GB -MAC 001337133704
+New-TalosVM -VMNamePrefix k8s-wk -CPUCount 8 -StartupMemory 24GB -SwitchName StagingWk -TalosISOPath "C:\Users\jgarf\Downloads\talos-amd64-v1.4.4.iso" -NumberOfVMs 1 -VMDestinationBasePath 'F:\Hyper-V\Talos' -VHDSize 50GB -StorageVHDSize 100GB -MAC 001337133704
 # New-TalosVM -VMNamePrefix k8s-wk -CPUCount 4 -StartupMemory 8GB -SwitchName StagingWk -TalosISOPath "C:\Users\jgarf\Downloads\talos-amd64-v1.4.4.iso" -NumberOfVMs 1 -VMDestinationBasePath 'F:\Hyper-V\Talos' -VHDSize 30GB -MAC 001337133705
 
 Start-VM -VMName k8s-cp01
