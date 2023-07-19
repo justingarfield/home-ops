865462eb9eb28a3ed6c099289d7e7f081cde90c4
Justin Garfield <github@jgarfield.com>
2023-07-19 09:28:57 -0400

    Start adding snapshot-controller bits2

--- scripts/hyper-v/configure-hyper-v-cluster.ps1
+++ scripts/hyper-v/configure-hyper-v-cluster.ps1
@@ -158,7 +158,7 @@ Remove-Item 'F:\Hyper-V\Talos\k8s-wk02' -Recurse -ErrorAction SilentlyContinue
 New-TalosVM -VMNamePrefix k8s-cp -CPUCount 4 -StartupMemory 8GB -SwitchName StagingCp -TalosISOPath "C:\Users\jgarf\Downloads\talos-amd64-v1.4.6.iso" -NumberOfVMs 1 -VMDestinationBasePath 'C:\Hyper-V\Talos' -VHDSize 20GB -MAC 001337133701
 # New-TalosVM -VMNamePrefix k8s-cp -CPUCount 4 -StartupMemory 8GB -SwitchName StagingCp -TalosISOPath "C:\Users\jgarf\Downloads\talos-amd64-v1.4.6.iso" -NumberOfVMs 1 -VMDestinationBasePath 'C:\Hyper-V\Talos' -VHDSize 20GB -MAC 001337133702
 # New-TalosVM -VMNamePrefix k8s-cp -CPUCount 4 -StartupMemory 8GB -SwitchName StagingCp -TalosISOPath "C:\Users\jgarf\Downloads\talos-amd64-v1.4.6.iso" -NumberOfVMs 1 -VMDestinationBasePath 'C:\Hyper-V\Talos' -VHDSize 20GB -MAC 001337133703
-New-TalosVM -VMNamePrefix k8s-wk -CPUCount 8 -StartupMemory 24GB -SwitchName StagingWk1 -TalosISOPath "C:\Users\jgarf\Downloads\talos-amd64-v1.4.6.iso" -NumberOfVMs 1 -VMDestinationBasePath 'F:\Hyper-V\Talos' -VHDSize 50GB -StorageVHDSize 100GB -MAC 001337133704
-# New-TalosVM -VMNamePrefix k8s-wk -CPUCount 8 -StartupMemory 8GB -SwitchName StagingWk2 -TalosISOPath "C:\Users\jgarf\Downloads\talos-amd64-v1.4.6.iso" -NumberOfVMs 1 -VMDestinationBasePath 'F:\Hyper-V\Talos' -VHDSize 50GB -StorageVHDSize 100GB -MAC 001337133705
+New-TalosVM -VMNamePrefix k8s-wk -CPUCount 8 -StartupMemory 16GB -SwitchName StagingWk1 -TalosISOPath "C:\Users\jgarf\Downloads\talos-amd64-v1.4.6.iso" -NumberOfVMs 1 -VMDestinationBasePath 'F:\Hyper-V\Talos' -VHDSize 50GB -MAC 001337133704 -StorageVHDSize 20GB
+New-TalosVM -VMNamePrefix k8s-wk -CPUCount 4 -StartupMemory 8GB -SwitchName StagingWk2 -TalosISOPath "C:\Users\jgarf\Downloads\talos-amd64-v1.4.6.iso" -NumberOfVMs 1 -VMDestinationBasePath 'F:\Hyper-V\Talos' -VHDSize 50GB -MAC 001337133705
 
 Start-VM -VMName k8s-cp01
